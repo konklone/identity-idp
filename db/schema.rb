4e2bf5e2d9809b72d2557709f5127facbbaa122c
Jonathan Hooper <jonathan.hooper@gsa.gov>
2021-11-23 09:24:14 -0700

    Setup platform authentication
    **Why**: So that people can use their platform authenticator devices to access login.gov

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_174749) do
+ActiveRecord::Schema.define(version: 2021_11_23_161315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -653,6 +653,7 @@ ActiveRecord::Schema.define(version: 2021_11_04_174749) do
     t.text "credential_public_key", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "platform_authenticator"
     t.index ["user_id"], name: "index_webauthn_configurations_on_user_id"
   end
 
